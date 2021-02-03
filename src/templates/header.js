8231946c4d4b933b5167f37b8cea702e3b0f3340
kevinmora47 <kevinmora47@gmail.com>
2021-02-02 19:03:21 -0600

    header view

---
+++ src/templates/header.js
@@ -0,0 +1,21 @@
+const header = () => {
+  const view = `
+    <div class="header">
+        <div class"header-logo>
+            <h1>
+            <a href="/">
+            Hello
+            </a>
+            </h1>
+        </div>
+        <div class="header-nav">
+            <a href="#/about">
+                About
+            </a>
+        </div>
+    </div>
+    `;
+  return view;
+};
+
+export default header;
