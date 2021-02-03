5741aefd6531fdd7d58f057b0bce6e993e153092
kevinmora47 <kevinmora47@gmail.com>
2021-02-02 19:10:43 -0600

    Character

---
+++ src/pages/Character.js
@@ -0,0 +1,22 @@
+const Character = () => {
+  const view = `    
+    <div class="characters-inner>    
+        <article class="character-card">
+            <img src="" alt="name">
+                <h2>Name</h2>
+            </img>
+        </article>
+        <article class="characters-card">
+            <h3>Episodios</h3>
+            <h3>Status</h3>
+            <h3>Especies</h3>
+            <h3>Genero</h3>
+            <h3>Origen</h3>
+            <h3>Ultima Ubicacion</h3>
+        </article>
+    </div>
+    `;
+  return view;
+};
+
+export default Character;
